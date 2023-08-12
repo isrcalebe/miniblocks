2c629e4eca6a66196e0b3cc05563ee7dac3450a5
isrcalebe <morshu0@proton.me>
2023-08-12 12:10:43 -0300

    feat(api/core): add base classes

---
+++ api/core/Object.cs
@@ -0,0 +1,12 @@
+// Part of the MINIBLOCKS, under the MIT License.
+// See COPYING for license information.
+// SPDX-License-Identifier: MIT
+
+namespace miniblocks.API.Core;
+
+/// <summary>
+/// The ultimate base class of all MINIBLOCKS classes.
+/// </summary>
+public class Object
+{
+}
